5e5fa8901a0f5f2a5a32471566bb44b5168b6719
vinodkumar <evinodkumar211@gmail.com>
2024-12-24 23:43:40 +0530

    initial commit

---
+++ Divsly/src/test/java/com/divsly/pageobjects/SignInPage.java
@@ -0,0 +1,67 @@
+package com.divsly.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignInPage {
+
+	WebDriver ldriver;
+
+	public SignInPage(WebDriver rdriver) {
+		ldriver = rdriver;
+		PageFactory.initElements(rdriver, this);
+	}
+
+	@FindBy(xpath = "//input[@type='email']")
+	WebElement inputUserEmail;
+
+	@FindBy(xpath = "//input[@type='password']")
+	WebElement inputUserPassword;
+
+	@FindBy(xpath = "//button[text()='Continue']")
+	WebElement buttonContinue;
+
+	@FindBy(linkText = "/signup")
+	WebElement linkSignUp;
+
+	@FindBy(linkText = "/forgotPassword")
+	WebElement linkForgotPassword;
+
+	@FindBy(xpath = "//span[text()='Sign in with Google']")
+	WebElement buttonSignInWithGoogle;
+
+	@FindBy(xpath = "//span[text()='Please enter a valid email']")
+	WebElement textenteraValidEmail;
+
+	@FindBy(xpath = "//span[text()='Account not found.']")
+	WebElement textAccountNotFound;
+
+	public void enterEmailAddress(String email) {
+		inputUserEmail.clear();
+		inputUserEmail.sendKeys(email);
+	}
+
+	public void enterPasswor(String password) {
+		inputUserPassword.clear();
+		inputUserPassword.sendKeys(password);
+	}
+
+	public void clickSignin() {
+		buttonContinue.click();
+	}
+
+	public void clickSignup() {
+		linkSignUp.click();
+	}
+
+	public void clickForgotPassword() {
+		linkForgotPassword.click();
+	}
+
+	public void clickSigninWithGoogle() {
+		buttonSignInWithGoogle.click();
+	}
+
+}
